b1511a50cd48e97758aab117665833ee952a2bf1
cain <cain@qq.com>
2020-05-25 17:07:47 +0800

    ss
    sd

--- app/src/main/java/com/cain/ui/MainActivity.java
+++ app/src/main/java/com/cain/ui/MainActivity.java
@@ -1,15 +1,249 @@
 package com.cain.ui;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+import butterknife.BindView;
+
 
 public class MainActivity extends Activity {
+
+    private LinearLayout mLLMenu;
+    private String[] mMenuTitles;
+    private Drawable[] mMenuIcons;
+
+    @BindView(R.id.tabs)
+    TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
     }
 
+    private void initViews() {
+        //initTab();
+
+        //静默检查版本更新
+        //Utils.checkUpdate(this, false);
+    }
+
+    /**
+     * 初始化Tab
+     */
+    private void initTab() {
+        TabLayout.Tab component = mTabLayout.newTab();
+        component.setText("组件");
+        component.setIcon(R.drawable.selector_icon_tabbar_component);
+        mTabLayout.addTab(component);
+
+        TabLayout.Tab util = mTabLayout.newTab();
+        util.setText("工具");
+        util.setIcon( R.drawable.selector_icon_tabbar_util);
+        mTabLayout.addTab(util);
+
+        TabLayout.Tab expand = mTabLayout.newTab();
+        expand.setText("拓展");
+        expand.setIcon(R.drawable.selector_icon_tabbar_expand);
+        mTabLayout.addTab(expand);
+/*
+        switchPage(ComponentsFragment.class);
+
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mAdapter.setSelected(tab.getPosition());
+                switch (tab.getPosition()) {
+                    case POS_COMPONENTS:
+                        switchPage(ComponentsFragment.class);
+                        break;
+                    case 1:
+                        switchPage(UtilitysFragment.class);
+                        break;
+                    case 2:
+                        switchPage(ExpandsFragment.class);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });*/
+    }
+
+
+   /* private void initSlidingMenu(Bundle savedInstanceState) {
+        mMenuTitles = ResUtils.getStringArray(R.array.menu_titles);
+        mMenuIcons = ResUtils.getDrawableArray(this, R.array.menu_icons);
+
+        mSlidingRootNav = new SlidingRootNavBuilder(this)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.menu_left_drawer)
+                .inject();
+
+        mLLMenu = mSlidingRootNav.getLayout().findViewById(R.id.ll_menu);
+        final AppCompatImageView ivQrcode = mSlidingRootNav.getLayout().findViewById(R.id.iv_qrcode);
+        ivQrcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewPage(QRCodeFragment.class);
+            }
+        });
+
+        final AppCompatImageView ivSetting = mSlidingRootNav.getLayout().findViewById(R.id.iv_setting);
+        ivSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewPage(SettingFragment.class);
+            }
+        });
+
+        mAdapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(POS_COMPONENTS).setChecked(true),
+                createItemFor(POS_UTILITYS),
+                createItemFor(POS_EXPANDS),
+                createItemFor(POS_ABOUT),
+                new SpaceItem(48),
+                createItemFor(POS_LOGOUT)));
+        mAdapter.setListener(this);
+
+        RecyclerView list = findViewById(R.id.list);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(mAdapter);
+
+        mAdapter.setSelected(POS_COMPONENTS);
+        mSlidingRootNav.setMenuLocked(false);
+        mSlidingRootNav.getLayout().addDragStateListener(new DragStateListener() {
+            @Override
+            public void onDragStart() {
+
+            }
+
+            @Override
+            public void onDragEnd(boolean isMenuOpened) {
+                if (isMenuOpened) {
+                    if (!GuideCaseView.isShowOnce(MainActivity.this, getString(R.string.guide_key_sliding_root_navigation))) {
+                        final GuideCaseView guideStep1 = new GuideCaseView.Builder(MainActivity.this)
+                                .title("点击进入，可切换主题样式哦～～")
+                                .titleSize(18, TypedValue.COMPLEX_UNIT_SP)
+                                .focusOn(ivSetting)
+                                .build();
+
+                        final GuideCaseView guideStep2 = new GuideCaseView.Builder(MainActivity.this)
+                                .title("点击进入，扫码关注哦～～")
+                                .titleSize(18, TypedValue.COMPLEX_UNIT_SP)
+                                .focusOn(ivQrcode)
+                                .build();
+
+                        new GuideCaseQueue()
+                                .add(guideStep1)
+                                .add(guideStep2)
+                                .show();
+                        GuideCaseView.setShowOnce(MainActivity.this, getString(R.string.guide_key_sliding_root_navigation));
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+        switch (position) {
+            case POS_COMPONENTS:
+            case POS_UTILITYS:
+            case POS_EXPANDS:
+                if (mTabLayout != null) {
+                    TabLayout.Tab tab = mTabLayout.getTabAt(position);
+                    if (tab != null) {
+                        tab.select();
+                    }
+                }
+                mSlidingRootNav.closeMenu();
+                break;
+            case POS_ABOUT:
+                openNewPage(AboutFragment.class);
+                break;
+            case POS_LOGOUT:
+                DialogLoader.getInstance().showConfirmDialog(
+                        this,
+                        getString(R.string.lab_logout_confirm),
+                        getString(R.string.lab_yes),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                TokenUtils.handleLogoutSuccess();
+                                finish();
+                            }
+                        },
+                        getString(R.string.lab_no),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }
+                );
+                break;
+            default:
+                break;
+        }
+    }
+
+    private DrawerItem createItemFor(int position) {
+        return new SimpleItem(mMenuIcons[position], mMenuTitles[position])
+                .withIconTint(ThemeUtils.resolveColor(this, R.attr.xui_config_color_content_text))
+                .withTextTint(ThemeUtils.resolveColor(this, R.attr.xui_config_color_content_text))
+                .withSelectedIconTint(ThemeUtils.resolveColor(this, R.attr.colorAccent))
+                .withSelectedTextTint(ThemeUtils.resolveColor(this, R.attr.colorAccent));
+    }
+
+    *//**
+     * 菜单、返回键响应
+     *//*
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (isMenuOpen()) {
+                closeMenu();
+            } else {
+                ClickUtils.exitBy2Click(2000, this);
+            }
+        }
+        return true;
+    }
+
+    *//**
+     * 再点击一次
+     *//*
+    @Override
+    public void onRetry() {
+        XToastUtils.toast("再按一次退出程序");
+    }
+
+    *//**
+     * 退出
+     *//*
+    @Override
+    public void onExit() {
+        moveTaskToBack(true);
+    }*/
 
 }
