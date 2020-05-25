0bd16b182e6167e839dfe96ecad77e24ffffb79b
cain007 <cain2009@163.com>
2020-05-25 22:33:44 +0800

    引入 androidx

--- app/src/main/java/com/cain/ui/MainActivity.java
+++ app/src/main/java/com/cain/ui/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity {
     }
 
     private void initViews() {
-        //initTab();
+        initTab();
 
         //静默检查版本更新
         //Utils.checkUpdate(this, false);
